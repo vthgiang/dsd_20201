265fd92293be0c6dd2b32913a84b430536486495
Sieusaosieungo <thinhhanh1@gmail.com>
2020-11-11 23:34:49 +0700

    add route and icon to menu

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,21 +1,77 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Dashboard from "./Dashboard";
+import Dashboard from './Dashboard';
 
 export const routes = [
   {
-    path: "/dashboard",
+    path: '/dashboard',
     component: Dashboard,
     exact: true,
   },
   {
-    path: "/drones",
+    path: '/drones',
     component: () => <div>DroneManagement</div>,
   },
   {
-    path: "/payloads",
+    path: '/drone-state',
+    component: () => <div>Tình trạng drone</div>,
+  },
+  {
+    path: '/fly-setting',
+    component: () => <div>Thiết lập đường bay</div>,
+  },
+  {
+    path: '/drone-statistic',
+    component: () => <div>Thống kê drone</div>,
+  },
+  {
+    path: '/flight-hub',
+    component: () => <div>Flight Hub</div>,
+  },
+  {
+    path: '/payloads',
     component: () => <div>PayloadManagement</div>,
+  },
+  {
+    path: '/metadata',
+    component: () => <div>Meta Data</div>,
+  },
+  {
+    path: '/problem',
+    component: () => <div>Problem</div>,
+  },
+  {
+    path: '/supervised-object',
+    component: () => <div>Đối tượng giám sát</div>,
+  },
+  {
+    path: '/statistic',
+    component: () => <div>Báo cáo thống kê</div>,
+  },
+  {
+    path: '/problems',
+    component: () => <div>Sự cố</div>,
+  },
+  {
+    path: '/warning',
+    component: () => <div>Cảnh báo</div>,
+  },
+  {
+    path: '/activity-log',
+    component: () => <div>Lịch sử hoạt động</div>,
+  },
+  {
+    path: '/surveillance-domain',
+    component: () => <div>Miền giám sát</div>,
+  },
+  {
+    path: '/handle-problem',
+    component: () => <div>Xử lý sự cố</div>,
+  },
+  {
+    path: '/user-management',
+    component: () => <div>Quản lý người dùng</div>,
   },
 ];
 
@@ -32,6 +88,6 @@ export default () => (
         />
       );
     })}
-    <Redirect to="/dashboard" />
+    <Redirect to='/dashboard' />
   </Switch>
 );
