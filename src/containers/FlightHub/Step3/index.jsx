bf8b250132e07f245c197eb15310aeeb557db471
taipd-vnist <taipd.vnist@gmail.com>
2020-11-16 16:17:31 +0700

    step 3-4

--- src/containers/FlightHub/Step3/index.jsx
+++ src/containers/FlightHub/Step3/index.jsx
@@ -1,12 +1,112 @@
-import React from 'react'
-import StyleStep3 from "./index.style";
+import React from 'react';
+import StyleStep3 from './index.style';
+import { Button, Col, Form, Select, Row } from 'antd';
+import { VALIDATE_MESSAGES, LAYOUT } from '../config';
 
-const Step3 = () => {
+const { Option } = Select;
+
+const Step3 = ({ nextStep, prevStep, handleChangeData }) => {
+  const onFinish = (values) => {
+    handleChangeData(values);
+    nextStep();
+  };
+
+  const getObjectOptions = () => {
+    let data = [
+      {
+        _id: '1',
+        name: 'Rừng đặc dụng',
+      },
+      {
+        _id: '2',
+        name: 'Rừng nguyên sinh',
+      },
+      {
+        _id: '3',
+        name: 'Rừng phòng hộ',
+      },
+    ];
+
+    const options = data.map((item) => {
+      return <Option value={item._id}>{item.name}</Option>;
+    });
+    return options;
+  };
+
+  const getLocationOptions = () => {
+    let data = [
+      {
+        _id: '1',
+        name: 'Tiểu khu A',
+      },
+      {
+        _id: '2',
+        name: 'Tiểu khu B',
+      },
+      {
+        _id: '3',
+        name: 'Tiểu khu C',
+      },
+    ];
+
+    const options = data.map((item) => {
+      return <Option value={item._id}>{item.name}</Option>;
+    });
+    return options;
+  };
+
   return (
     <StyleStep3>
-      StyleStep3
+      <Form
+        {...LAYOUT}
+        name="flight-hub-name"
+        onFinish={onFinish}
+        validateMessages={VALIDATE_MESSAGES}
+      >
+        <Form.Item
+          name="object_id"
+          label="Đối tượng giám sát"
+          rules={[{ type: 'string', required: true }]}
+        >
+          <Select
+            showSearch
+            style={{ width: 300 }}
+            placeholder="Chọn đối tượng giám sát"
+          >
+            {getObjectOptions()}
+          </Select>
+        </Form.Item>
+        ,
+        <Form.Item
+          name="location"
+          label="Miền giám sát"
+          rules={[{ type: 'string', required: true }]}
+        >
+          <Select
+            showSearch
+            style={{ width: 300 }}
+            placeholder="Chọn miền giám sát"
+          >
+            {getLocationOptions()}
+          </Select>
+        </Form.Item>
+        <Col offset={6}>
+          <Row>
+            <Button
+              type="primary"
+              onClick={prevStep}
+              style={{ marginRight: '20px' }}
+            >
+              {'<< quay lại'}
+            </Button>
+            <Button type="primary" htmlType="submit">
+              {'Tiếp theo >>'}
+            </Button>
+          </Row>
+        </Col>
+      </Form>
     </StyleStep3>
-  )
-}
+  );
+};
 
-export default Step3
+export default Step3;
--- src/containers/FlightHub/Step4/index.jsx
+++ src/containers/FlightHub/Step4/index.jsx
@@ -1,12 +1,135 @@
-import React from 'react'
-import StyleStep4 from "./index.style";
+import React from 'react';
+import StyleStep4 from './index.style';
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Select,
+  Row,
+  Popconfirm,
+  message,
+} from 'antd';
+import { VALIDATE_MESSAGES, LAYOUT } from '../config';
 
-const Step4 = () => {
+const { Option } = Select;
+const { TextArea } = Input;
+
+const Step4 = ({ nextStep, prevStep, handleChangeData }) => {
+  const onFinish = (values) => {
+    handleChangeData(values);
+  };
+
+  const save = () => {
+    message.success('Lưu thành công');
+  };
   return (
     <StyleStep4>
-      StyleStep4
+      <Form
+        {...LAYOUT}
+        name="flight-hub-name"
+        onFinish={onFinish}
+        validateMessages={VALIDATE_MESSAGES}
+      >
+        <Form.Item
+          name="mechanism"
+          label="Cơ chế thu thập"
+          rules={[{ type: 'string', required: true }]}
+        >
+          <Select
+            showSearch
+            style={{ width: 300 }}
+            placeholder="Chọn cơ chế thu thập"
+          >
+            <Option value="manually">Thủ công</Option>
+            <Option value="auto">Tự động</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name="metadataType"
+          label="Dạng lưu trữ"
+          rules={[{ type: 'string', required: true }]}
+        >
+          <Select
+            showSearch
+            style={{ width: 300 }}
+            placeholder="Chọn dạng lưu trữ"
+          >
+            <Option value="photo">Hình ảnh</Option>
+            <Option value="video">Video</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name="resolution"
+          label="Độ phân giải"
+          rules={[{ type: 'string', required: true }]}
+        >
+          <Select
+            showSearch
+            style={{ width: 300 }}
+            placeholder="Chọn độ phân giải"
+          >
+            <Option value="480p">480p</Option>
+            <Option value="720p">720p</Option>
+            <Option value="1080p">1080p</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name="attachParams"
+          label="Tham số đính kèm"
+          rules={[{ type: 'array', required: true }]}
+        >
+          <Select
+            showSearch
+            style={{ width: 300 }}
+            mode="tags"
+            placeholder="Chọn Các tham số đính kèm"
+          >
+            <Option value="uav_source">Nguồn UAV</Option>
+            <Option value="time">Thời gian</Option>
+            <Option value="coordinate">Tọa độ</Option>
+            <Option value="location">Vị trí</Option>
+            <Option value="journeys">Hành trình</Option>
+            <Option value="weather">Thời tiết</Option>
+            <Option value="temperature">Nhiệt độ</Option>
+            <Option value="humidity">Độ ẩm</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          name="description"
+          label="Ghi chú"
+          rules={[{ type: 'string', required: false }]}
+        >
+          <TextArea
+            placeholder="Gõ vào đây bất cứ ghi chú nào bạn muốn nhắn nhủ..."
+            autoSize={{ minRows: 2, maxRows: 6 }}
+          />
+        </Form.Item>
+        <Col offset={6}>
+          <Row>
+            <Button
+              type="primary"
+              onClick={prevStep}
+              style={{ marginRight: '20px' }}
+            >
+              {'<< quay lại'}
+            </Button>
+            <Popconfirm
+              title="Bạn có chắc chắn muốn lưu không?"
+              onConfirm={save}
+              // onCancel={cancel}
+              okText="Lưu"
+              cancelText="Hủy"
+            >
+              <Button type="primary" htmlType="submit">
+                {'Lưu'}
+              </Button>
+            </Popconfirm>
+          </Row>
+        </Col>
+      </Form>
     </StyleStep4>
-  )
-}
+  );
+};
 
-export default Step4
+export default Step4;
