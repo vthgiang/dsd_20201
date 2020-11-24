9238a2fb6aefa3cf7323ef3ef2d92189a9138e65
uyenttt <tranthuuyen.nt@gmail.com>
2020-11-25 06:38:48 +0700

    updat code add, update payload

--- src/pages/Payload/PayloadManagement/List/index.jsx
+++ src/pages/Payload/PayloadManagement/List/index.jsx
@@ -17,10 +17,13 @@ class List extends Component {
       visible: false,
       currentTable: null,
       tables: [],
-      idPayload : null,
+      idPayload: null,
       modalLoading: false,
       detailBill: [],
-      detailPayload : null
+      detailPayload: {},
+      options: [],
+      payload: {},
+      visibleAdd: false,
     }
   }
 
@@ -33,14 +36,19 @@ class List extends Component {
     axios.get(`http://dsd06.herokuapp.com/api/payload`)
       .then(res => {
         //const persons = res.data;
-        this.setState({ detailPayload: res.data });
+        this.setState({ tables: res.data });
       })
+
+      this.getAllTypePayload();
   }
 
-  showModal = (record) => {
 
+
+  showModal = (record) => {
     this.setState({ visible: true });
-    this.setState({idPayload: record.id})
+    
+    this.setState({ detailPayload: record });
+    this.getDetailPayload(record.id);
   };
 
   handleOk = e => {
@@ -50,6 +58,13 @@ class List extends Component {
   handleCancel = e => {
     this.setState({ visible: false })
   };
+
+  
+  handleCancelAdd = e => {
+    this.setState({ visibleAdd: false })
+  };
+
+  
 
   handleSubmit = event => {
     event.preventDefault();
@@ -65,17 +80,49 @@ class List extends Component {
       })
   }
 
-  renderModal = () => {
-    axios.get(`http://dsd06.herokuapp.com/api/payload/` + this.state.idPayload)
+  getAllTypePayload() {
+    axios.get(`https://dsd06.herokuapp.com/api/payloadtype`)
+      .then(res => {
+        const options = res.data.map(payload =>
+          ({
+            label: payload.name,
+            value: payload._id,
+          })
+        )
+        //const persons = res.data;
+        this.setState({ Options: options });
+      })
+    //console.log(this.state.Options.length)
+    //alert(this.state.Options)
+  }
+
+  getDetailPayload(id) {
+    axios.get(`http://dsd06.herokuapp.com/api/payload/` + id)
       .then(res => {
         //const persons = res.data;
-        this.setState({ tables: res.data });
+        this.setState({ payload: res.data });
       })
+  }
 
-    return <Form onSubmit={this.handleSubmit} >
+  renderModal() {
+    //this.getDetailPayload();
+    //alert(this.state.detailPayload.id)
+    //alert(this.state.detailPayload.id)
+    //console.log(this.state.options.length)
+
+    //alert(option.length)
+    return <Form onSubmit={this.handleSubmit}  >
+
+      <Form.Item
+        label="Mã" onChange={this.handleChange}
+        name="code" initialValue={this.state.detailPayload.code}
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
       <Form.Item
         label="Tên"
-        name="username"  initialValue="123"
+        name="username" initialValue={this.state.detailPayload.name}
         rules={[{ required: true, message: 'Please input your username!' }]}
       >
         <Input />
@@ -86,23 +133,16 @@ class List extends Component {
         name="tilting"
         rules={[{ required: true, message: 'Please input your password!' }]}
       >
-        <Input />
+
+        <Select options={this.state.Options} defaultValue={this.state.detailPayload.type} />
+
       </Form.Item>
       <Form.Item
         label="Mô tả"
-        name="zooming"
+        name="des" initialValue={this.state.detailPayload.des}
         rules={[{ required: true, message: 'Please input your password!' }]}
       >
         <Input />
-      </Form.Item>
-      <Form.Item
-        label="Trạng thái"
-        name="zooming"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Select>
-          <Select.Option value="demo">Demo</Select.Option>
-        </Select>
       </Form.Item>
       <Form.Item>
         <Button type="primary" htmlType="submit">
@@ -113,24 +153,176 @@ class List extends Component {
 
   }
 
+  showModalAdd() {
+    this.setState({ visibleAdd: true });
+  }
+
+  renderModalAdd() {
+    const layout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 10 },
+    };
+    return <Form   {...layout}  >
+
+      <Row gutter={16}>
+        <Col className="gutter-row" span={12}>
+          <Form.Item
+            label="Mã"
+            name="code"
+            rules={[{ required: true, message: 'Please input your mã!' }]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <Form.Item
+            label="Tên"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col className="gutter-row" span={12}>
+          <Form.Item
+            label="Loại"
+            name="tilting"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Select options={this.state.Options} />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <Form.Item
+            label="Mô tả"
+            name="des"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="Weight">
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="Nhà sản xuất">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="OpticalZoom">
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="DigitalZoom">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      
+      <Row gutter={16}>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="Panning min">
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="Panning max">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="Tilting min">
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="Tilting max">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="Zooming min">
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <Form.Item label="Zooming max">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col className="gutter-row" span={8}>
+          <Form.Item label="Width">
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={8}>
+          <Form.Item label="Height">
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" span={8}>
+          <Form.Item label="Length">
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+   </Button>
+      </Form.Item>
+    </Form>
+  }
+
   render() {
+
+    const dataSource = this.state.tables.map(payload =>
+      ({
+        id: payload._id,
+        code: payload.code,
+        name: payload.name,
+        status: payload.status,
+        manufacturer: payload.detail.manufacturer,
+        type: payload.type.name,
+        des: payload.type.description,
+        type_id: payload.type._id,
+      })
+    )
+
 
 
     const columns = [
       {
-        title: 'STT',
-        dataIndex: 'stt',
-        key: 'stt',
-      },
-      {
         title: 'Mã ',
-        dataIndex: 'ID',
-        key: 'ID',
+        dataIndex: 'code',
+        key: 'code',
       },
       {
         title: 'Tên',
         dataIndex: 'name',
         key: 'name',
+      },
+      {
+        title: 'Nhà sản xuất',
+        dataIndex: 'manufacturer',
+        key: 'manufacturer',
       },
       {
         title: 'Loại',
@@ -161,33 +353,8 @@ class List extends Component {
         ),
       },
     ];
-    const dataSource = [
-      {
-        stt: '1',
-        ID: 'TT73623',
-        name: 'Sensor T',
-        type: 'Sensor',
-        des: '',
-        status: ''
-      },
-      {
-        stt: '2',
-        ID: 'PN62523',
-        name: 'Camera cảm biến nhiệt',
-        type: 'Camera',
-        des: '',
-        status: ''
-      },
-      {
-        stt: '3',
-        ID: 'SN04628',
-        name: 'Camera SONY 04628',
-        type: 'Camera',
-        des: '',
-        status: ''
-      },
-    ];
-    const { visible, currentTable, tables } = this.state;
+
+    const { visible, visibleAdd, currentTable, tables } = this.state;
 
     return (
       <StyleList>
@@ -224,7 +391,7 @@ class List extends Component {
               </Col>
             </Row>
           </Form>
-          <Button type="primary" className="buttontype" >Thêm</Button>
+          <Button type="primary" className="buttontype" onClick={() => this.showModalAdd()} >Thêm</Button>
           <Table dataSource={dataSource} columns={columns} />;
         </div>
         <Modal
@@ -234,9 +401,21 @@ class List extends Component {
           onCancel={this.handleCancel}
           footer={null}
         >
-          {/* {
+          {
             this.renderModal()
-          } */}
+          }
+        </Modal>
+
+        <Modal
+          title="Thêm Payload" width={800}
+          visible={visibleAdd}
+          onOk={this.handleOkAdd}
+          onCancel={this.handleCancelAdd}
+          footer={null}
+        >
+          {
+            this.renderModalAdd()
+          }
         </Modal>
       </StyleList>
     );
