f48b30903d4bc99e9341e23077b54ed87695652d
Duy Cao Van <caovanduy15@gmail.com>
2020-11-24 17:33:21 +0700

    Add data

--- src/components/Group4/Notification/index.jsx
+++ src/components/Group4/Notification/index.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { List, ListItem, ListItemAvatar, Avatar, ListItemText, TablePagination, Button } from '@material-ui/core'
-import datas from './contacts.json'
+// import datas from './contacts.json'
 import { ListItemStyle, ButtonStyle } from './index.style'
 import Dialog from "../Dialog";
+import { IMAGES } from '../../../constants';
+import { InvertColorsOff, SentimentSatisfied } from '@material-ui/icons';
+
+var axios = require('axios');
 
 const MyList = () => {
+
+    const [datas, setDatas] = useState([]);
+    const isLoad = false;
+    useEffect(() => {
+        if(!isLoad) {
+        var config = {
+            method: 'get',
+            url: 'https://it4483-dsd04.herokuapp.com/get_list_ntf',
+            headers: {
+                'Content-Type': 'application/json',
+                'api-token': '1fa6b94047ba20d998b44ff1a2c78bba',
+                'project-type': 'CHAY_RUNG'
+            }
+            };
+
+            axios(config)
+            .then(function (response) {
+                setDatas(response.data.data);
+                console.log(datas);
+            })
+            .catch(function (error) {
+            console.log(error);
+            });
+        }
+    })
+
+
+
     const [open, setOpen] = React.useState(false);
     //const [selectedValue, setSelectedValue] = React.useState(emails[1]);
     const [page, setPage] = React.useState(0);
@@ -29,11 +61,11 @@ const MyList = () => {
             {datas.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(data => (
                 <ListItemStyle onClick={handleClickOpen}>
                     <ListItemAvatar className="name">
-                        <Avatar src="https://vtv1.mediacdn.vn/zoom/550_339/2020/9/29/mi290920-16013769565851267817310.jpg">
+                        <Avatar src='/images/tree.jpg'>
                             {data.image}
                         </Avatar>
                     </ListItemAvatar>
-                    <ListItemText primary={data.name} secondary={data.email} />
+                    <ListItemText primary={data.content} secondary={data.level} />
 
                 </ListItemStyle>
             ))}
---
+++ src/components/Group4/config4.js
@@ -0,0 +1,16 @@
+import { PUBLIC_URL } from '../configs';
+
+const ref = {
+    prop: {
+        1: {name: 'Drone', value: 1, img: `${PUBLIC_URL}/images/drone.png`},
+        2: {name: 'Payload', value: 2, img: `${PUBLIC_URL}/images/drone.png`},
+        3: {name: 'User', value: 3, img: `${PUBLIC_URL}/images/drone.png`},
+        4: {name: 'Image/Video', value: 4, img: `${PUBLIC_URL}/images/drone.png`},
+        5: {name: 'Đối tượng giám sát', value: 5, img: `${PUBLIC_URL}/images/drone.png`},
+        6: {name: 'Báo cáo thống kê', value: 6, img: `${PUBLIC_URL}/images/drone.png`},
+        7: {name: 'Miền giám sát', value: 7, img: `${PUBLIC_URL}/images/drone.png`},
+        8: {name: 'Công việc xử lý sự cố', value: 8, img: `${PUBLIC_URL}/images/drone.png`},
+        9: {name: 'Flighthub', value: 9, img: `${PUBLIC_URL}/images/drone.png`},
+        10: {name: 'Sư cố lưới điện', value: 1, img: `${PUBLIC_URL}/images/drone.png`}
+    }
+}
