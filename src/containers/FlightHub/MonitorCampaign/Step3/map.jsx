a498abb286bda499c82d5823fc1b47097bb1ec79
phamdaitai <phamdaitai9899@gmail.com>
2020-12-01 12:07:26 +0700

    mien giam sat

--- src/containers/FlightHub/MonitorCampaign/Step3/map.jsx
+++ src/containers/FlightHub/MonitorCampaign/Step3/map.jsx
@@ -5,27 +5,49 @@ import {
   withGoogleMap,
   Marker,
   InfoWindow,
+  Rectangle,
 } from 'react-google-maps';
 import { removeVietnameseTones } from '../../../../helpers/removeVietnameseTones';
 import parksData from './data.json';
 
 import { Input } from 'antd';
+import { HeatMapOutlined } from '@ant-design/icons';
+const axios = require('axios');
 const { Search } = Input;
 
 const Map = ({ onChangeLocation, parkIdInit }) => {
-  const [selectedPark, setSelectedPark] = useState(null);
-  const [dataPark, setDataPark] = useState([]);
+  const [monitoredZones, setMonitoredZones] = useState(null);
+  const [selectedMonitoredZone, setSelectedMonitoredZone] = useState(null);
+  const [positionClick, setPositionClick] = useState(null);
   const [searchText, searchTextValue] = useState('');
 
   useEffect(() => {
-    setDataPark(parksData);
+    // setDataPark(parksData);
 
     //khởi tạo park nếu đã có sẵn
-    if (parkIdInit) {
-      let park = parksData.find((element) => element.PARK_ID == parkIdInit);
-      handleParkChange(park);
-    }
+    // if (parkIdInit) {
+    //   let park = parksData.find((element) => element.PARK_ID == parkIdInit);
+    //   handleParkChange(park);
+    // }
+
+    getMonitoredZone();
   }, []);
+
+  const getMonitoredZone = async () => {
+    await axios({
+      method: 'GET',
+      url: `https://monitoredzoneserver.herokuapp.com/area`,
+    })
+      .then((res) => {
+        if (res.data) {
+          setMonitoredZones(res.data.content.monitoredArea);
+          console.log(res.data.content.monitoredArea);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const searchOnChange = (e) => {
     searchTextValue(e.target.value);
@@ -40,19 +62,28 @@ const Map = ({ onChangeLocation, parkIdInit }) => {
       }
     });
 
-    setDataPark(data);
+    // setDataPark(data);
   };
 
   const handleParkChange = (park) => {
-    setSelectedPark(park);
+    // setSelectedPark(park);
     searchTextValue(park.NAME);
     onChangeLocation(park);
   };
 
+  const handleClickMonitoredZones = (zone, e) => {
+    setPositionClick({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+    setSelectedMonitoredZone(zone);
+  };
+
+  const handleCloseMonitoredZones = () => {
+    setSelectedMonitoredZone(null);
+  };
+
   return (
     <GoogleMap
-      defaultZoom={14}
-      defaultCenter={{ lat: 21.003943, lng: 105.842716 }}
+      defaultZoom={0}
+      defaultCenter={{ lat: 40.712216, lng: -74.22655 }}
     >
       <Search
         placeholder="Nhập vào miền giám sát"
@@ -62,30 +93,79 @@ const Map = ({ onChangeLocation, parkIdInit }) => {
         style={{ position: 'absolute', top: '10px', left: '10px', width: 250 }}
         value={searchText}
       />
-      {dataPark.map((park) => (
-        <Marker
-          key={park.PARK_ID}
-          position={{
-            lat: park.geometry.coordinates[0],
-            lng: park.geometry.coordinates[1],
-          }}
-          onClick={() => handleParkChange(park)}
+      {/* {monitoredZones &&
+        monitoredZones.map((zone) => (
+          <Rectangle
+            defaultBounds={
+              new window.google.maps.LatLngBounds(
+                new window.google.maps.LatLng(
+                  zone.startPoint.latitude,
+                  zone.startPoint.longitude,
+                ),
+                new window.google.maps.LatLng(
+                  zone.endPoint.latitude,
+                  zone.endPoint.longitude,
+                ),
+              )
+            }
+          />
+        ))} */}
+
+      {monitoredZones && (
+        <Rectangle
+          defaultBounds={
+            new window.google.maps.LatLngBounds(
+              new window.google.maps.LatLng(
+                monitoredZones[0].startPoint.latitude,
+                monitoredZones[0].startPoint.longitude,
+              ),
+              new window.google.maps.LatLng(
+                monitoredZones[0].endPoint.latitude,
+                monitoredZones[0].endPoint.longitude,
+              ),
+            )
+          }
+          onClick={(e) => handleClickMonitoredZones(monitoredZones[0], e)}
         />
-      ))}
-      {selectedPark && (
+      )}
+
+      {selectedMonitoredZone && (
         <InfoWindow
           position={{
-            lat: selectedPark.geometry.coordinates[0],
-            lng: selectedPark.geometry.coordinates[1],
+            lat: positionClick.lat,
+            lng: positionClick.lng,
           }}
           onCloseClick={() => {
-            setSelectedPark(null);
+            handleCloseMonitoredZones();
           }}
         >
           <div>
-            <h3>{selectedPark.NAME}</h3>
-            <div>Vĩ độ: {selectedPark.geometry.coordinates[0]}</div>
-            <div>Kinh độ: {selectedPark.geometry.coordinates[1]}</div>
+            <h3>{selectedMonitoredZone.name}</h3>
+            <div>Mã miền g/s: {selectedMonitoredZone.code}</div>
+            <div>
+              Độ cao an toàn: &ensp;
+              <span style={{ color: 'red' }}>
+                {selectedMonitoredZone.minHeight}
+              </span>
+              -
+              <span style={{ color: 'red' }}>
+                {selectedMonitoredZone.maxHeight}
+              </span>
+              (m)
+            </div>
+            <div
+              style={{
+                marginTop: '10px',
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <button>
+                <HeatMapOutlined /> &ensp;
+                <a>Chọn miền g/s này</a>
+              </button>
+            </div>
           </div>
         </InfoWindow>
       )}
