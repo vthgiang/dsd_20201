34e924bac07e20c1321bf97401fdf33914b88e52
phamdaitai <phamdaitai9899@gmail.com>
2020-12-08 22:29:09 +0700

    label

--- src/apis/api.js
+++ src/apis/api.js
@@ -3,7 +3,7 @@ export const API_URL = 'https://flight-hub-api.herokuapp.com/';
 
 const instance = axios.create({
   baseURL: API_URL,
-  timeout: 1000,
+  timeout: 15000,
 });
 
 export default instance;
