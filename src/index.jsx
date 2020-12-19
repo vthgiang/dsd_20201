b253475b866c8a877389be3e60fdc9b8d1668ebc
Sieusaosieungo <thinhhanh1@gmail.com>
2020-12-19 23:42:12 +0700

    fix bug group 05

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 
-//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
