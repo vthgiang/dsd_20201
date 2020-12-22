b11750a3145806f0fb7b5f9c8e6041105f1dbd60
LeDinhPhuc <ledinhphucts98@gmail.com>
2020-12-22 22:04:01 +0700

    change api url

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,7 +1,7 @@
 const { PUBLIC_URL } = process.env;
 
 const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
-const API_DOMAIN = 'https://flight-hub-api.herokuapp.com/';
+const API_DOMAIN = 'http://14.248.5.197:16010/';
 const REQUEST_TIMEOUT = 15000;
 
 export { PUBLIC_URL, DATE_TIME_FORMAT, API_DOMAIN, REQUEST_TIMEOUT };
