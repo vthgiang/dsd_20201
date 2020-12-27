352ac0155044653402402d2b703eb54d830531ad
HUST\Quang.SHV167972 <quang.shv167972@sis.hust.edu.vn>
2020-12-27 15:36:38 +0700

    no message

--- src/pages/Home/DeDieu/Task5/index.jsx
+++ src/pages/Home/DeDieu/Task5/index.jsx
@@ -2,48 +2,26 @@ import React from "react";
 import { Tabs } from "antd";
 import { useSelector } from "react-redux";
 import ListUser from "./listUser";
-import ListDepartment from './listDepartment';
-import ListRole from './listRole';
-import ListPermission from './listPermission';
-import ListRolePermission from './listRolePermission'
-
-
+import ListDepartment from "./listDepartment";
+import ListRole from "./listRole";
+import ListPermission from "./listPermission";
+import ListRolePermission from "./listRolePermission";
 
 function callback(key) {
   console.log(key);
 }
 
 function Task5() {
-  const user = useSelector((state) => state.user.user);
-  const role = user.role;
+
   const { TabPane } = Tabs;
   return (
     <Tabs defaultActiveKey="1" onChange={callback}>
       <TabPane tab="Quản lý người dùng" key="1">
-        <ListUser/>
+        <ListUser />
       </TabPane>
       <TabPane tab="Quản lý phòng ban" key="2">
-        <ListDepartment/>
+        <ListDepartment />
       </TabPane>
-     {/* // {role == "ADMIN" ? ( */}
-        <TabPane tab="Quản lý chức vụ" key="3">
-          <ListRole />
-        </TabPane>
-      {/* ) : (
-        <TabPane tab="Quản lý chức vụ" key="3" disabled>
-          <ListRole />
-        </TabPane>
-      )} */}
-     
-        <TabPane tab="Quản lý quyền" key="4">
-          <ListPermission />
-        </TabPane>
-     
-    
-        <TabPane tab="Phân quyền" key="5">
-          <ListRolePermission />
-        </TabPane>
-    
     </Tabs>
   );
 }
