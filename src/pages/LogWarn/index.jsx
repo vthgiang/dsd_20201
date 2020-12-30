cd33e58658cbebf1e5d21512e48d9eb1f3f57539
DinhNT <compimprove@gmail.com>
2020-12-30 08:32:15 +0700

    .

--- src/pages/LogWarn/index.jsx
+++ src/pages/LogWarn/index.jsx
@@ -25,7 +25,7 @@ function App (props) {
 
   const fetchData = () => {
     setIsLoadedLogActivity(false);
-    let url = 'https://it4883logging.herokuapp.com/api/warning?';
+    let url = 'http://14.248.5.197:5012/api/warning?';
     let fromDate  = rangeTime.fromDate;
     let toDate = rangeTime.toDate;
 
